fc5b9ee78044ce93955132cc8d4458c80bd35ce2
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2018-02-20 10:34:13 +0530

    fix(uninstall): remove upgrade id from completed upgrades

--- uninstall.php
+++ uninstall.php
@@ -1,12 +1,13 @@
 <?php
-// Exit if access directly.
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+// if uninstall.php is not called by WordPress, die
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+	die;
 }
+
 
 $db_upgrades = give_get_completed_upgrades();
 
-if( isset( $db_upgrades['give_db_healthcheck_post_200_data'] ) ) {
-	unset( $db_upgrades['give_db_healthcheck_post_200_data'] );
+if ( in_array( 'give_db_healthcheck_post_200_data', $db_upgrades ) ) {
+	unset( $db_upgrades[ array_search( 'give_db_healthcheck_post_200_data', $db_upgrades ) ] );
 	update_option( 'give_completed_upgrades', $db_upgrades );
 }
