298b46a321836e834097bb20454831e1dfcbeb4c
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2018-08-13 12:46:01 +0530

    fix(db-upgrade): delete custom upgrades from option when plugin uninstall

--- uninstall.php
+++ uninstall.php
@@ -8,6 +8,18 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 $db_upgrades = give_get_completed_upgrades();
 
 if ( in_array( 'give_db_healthcheck_post_200_data', $db_upgrades ) ) {
-	unset( $db_upgrades[ array_search( 'give_db_healthcheck_post_200_data', $db_upgrades ) ] );
+	// Custom DB upgrades.
+	$db_upgrades = array(
+		'give_db_healthcheck_post_200_data',
+		'give_db_healthcheck_donation_donor',
+		'give_db_healthcheck_003_recover_old_paymentdata',
+		'give_db_healthcheck_220_recover_donationmeta'
+	);
+
+
+	foreach ( $db_upgrades as $db_upgrade ) {
+		unset( $db_upgrades[ array_search( $db_upgrade, $db_upgrades ) ] );
+	}
+
 	update_option( 'give_completed_upgrades', $db_upgrades );
 }
