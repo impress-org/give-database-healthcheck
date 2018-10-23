0b76d3680f091917c9410b04be24530bd1b28e1a
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2018-10-23 09:48:30 +0530

    fix: recover donation data from rcp plugin

--- admin/upgrades.php
+++ admin/upgrades.php
@@ -13,19 +13,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 function give_db_healthcheck_notices() {
 	Give_Updates::get_instance()->register(
 		array(
-			'id'       => 'give_dbh_delete_existing_donationmeta_table',
+			'id'       => 'give_dbh_recover_data_from_rcp',
 			'version'  => '0.0.3',
-			'callback' => 'give_dbh_delete_existing_donationmeta_table_callback',
-		)
-	);
-
-
-	Give_Updates::get_instance()->register(
-		array(
-			'id'       => 'give_db_healthcheck_003_recover_old_paymentdata',
-			'version'  => '0.0.3',
-			'callback' => 'give_db_healthcheck_003_recover_old_paymentdata_callback',
-			'depend'   => array( 'give_dbh_delete_existing_donationmeta_table' ),
+			'callback' => 'give_dbh_recover_data_from_rcp_callback',
 		)
 	);
 }
@@ -34,29 +24,14 @@ add_action( 'give_register_updates', 'give_db_healthcheck_notices' );
 
 
 /**
- * Remove corrupted data from give_donationmeta
- *
- * @since 0.0.3
- */
-function give_dbh_delete_existing_donationmeta_table_callback(){
-	global $wpdb;
-	$wpdb->query( "DELETE FROM {$wpdb->prefix}give_donationmeta WHERE donation_id IS NULL" );
-
-	give_set_upgrade_complete( 'give_dbh_delete_existing_donationmeta_table' );
-}
-
-
-/**
  * Recover old payment data
  *
  * @since 0.0.3
  */
-function give_db_healthcheck_003_recover_old_paymentdata_callback() {
+function give_dbh_recover_data_from_rcp_callback() {
 	global $wpdb;
 
-	$give_updates         = Give_Updates::get_instance();
-	$donation_id_col_name = Give()->payment_meta->get_meta_type() . '_id';
-	$donation_table       = Give()->payment_meta->table_name;
+	$give_updates = Give_Updates::get_instance();
 
 	// form query
 	$payments = new WP_Query( array(
@@ -74,48 +49,29 @@ function give_db_healthcheck_003_recover_old_paymentdata_callback() {
 		while ( $payments->have_posts() ) {
 			$payments->the_post();
 
-			$meta_data = $wpdb->get_results(
-				$wpdb->prepare(
-					"
-					SELECT * FROM $wpdb->postmeta
-					WHERE post_id=%d
+			// If form id exist then we do not need to recover data.
+			if ( give_get_payment_form_id( get_the_ID() ) ) {
+				continue;
+			}
+
+			$rcp_payment_meta = "{$wpdb->prefix}rcp_payment_meta";
+			$sql              = $wpdb->prepare(
+				"
+					SELECT * FROM {$rcp_payment_meta}
+					WHERE payment_id=%d
 					",
-					get_the_ID()
-				),
-				ARRAY_A
+				get_the_ID()
 			);
+
+			$meta_data = $wpdb->get_results( $sql, ARRAY_A );
 
 			if ( ! empty( $meta_data ) ) {
 				foreach ( $meta_data as $index => $data ) {
-					// ignore _give_payment_meta key.
-					if ( '_give_payment_meta' === $data['meta_key'] ) {
-						continue;
+					$status = Give()->payment_meta->update_meta( $data['payment_id'], $data['meta_key'], maybe_unserialize( $data['meta_value'] ) );
+
+					if ( $status ) {
+						$wpdb->query( "DELETE FROM {$rcp_payment_meta} WHERE meta_id={$data['meta_id']};" );
 					}
-
-					$is_duplicate_meta_key = $wpdb->get_results(
-						$wpdb->prepare(
-							"
-							SELECT * FROM {$donation_table}
-							WHERE meta_key=%s
-							AND {$donation_id_col_name}=%d
-							",
-							$data['meta_key'],
-							$data['post_id']
-						),
-						ARRAY_A
-					);
-
-					if ( $is_duplicate_meta_key ) {
-
-						continue;
-					}
-
-					$data[ $donation_id_col_name ] = $data['post_id'];
-
-					unset( $data['post_id'] );
-					unset( $data['meta_id'] );
-
-					Give()->payment_meta->insert( $data );
 				}
 			}
 
@@ -124,6 +80,6 @@ function give_db_healthcheck_003_recover_old_paymentdata_callback() {
 		wp_reset_postdata();
 	} else {
 		// No more forms found, finish up.
-		give_set_upgrade_complete( 'give_db_healthcheck_003_recover_old_paymentdata' );
+		give_set_upgrade_complete( 'give_dbh_recover_data_from_rcp' );
 	}
 }
